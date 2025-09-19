0608085b7fb6382f21c00493c989477a519b2be6
AnthonyHerve56 <herve.anthony.pro@gmail.com>
2025-09-19 13:56:34 +0200

    Création de la classe creer_un_csv

---
+++ Simulateur/Generer_un_csv.py
@@ -0,0 +1,13 @@
+class generer_un_csv:
+    '''
+        Classe pour générer un fichier csv
+    '''
+    def generer_le_titre(self):
+        '''
+            Fonctions pour créer les titres du csv et retourner la liste
+        '''
+        print("Générations des titre du csv")
+        liste_des_titres=['C1','C2','C3','C4']
+        
+        
+        return(liste_des_titres)
---
+++ Simulateur/Simulateur.py
@@ -0,0 +1,11 @@
+from Generer_un_csv import generer_un_csv
+
+
+if __name__ == "__main__":
+    print("Hello world")
+    test=generer_un_csv()
+    print(test.generer_le_titre())
+         
+
+    
+    
